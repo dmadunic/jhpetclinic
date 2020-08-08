e894f4e9aa0a634a6f7dde687f38fe6cdf244ee2
Domagoj Madunic <domagoj.madunic@gmail.com>
2020-08-08 21:33:40 +0200

    Add Loading bar (#2)
    * Added ngx-loading-bar npm packages as dependecies.
    
    * Configured Http Module Loadin bar
    
    * Registered and configured LoadingBarHttpClientModule

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -14,6 +14,9 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
 import { ErrorComponent } from './layouts/error/error.component';
+import { LoadingBarModule } from '@ngx-loading-bar/core';
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
+import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
 
 @NgModule({
   imports: [
@@ -24,6 +27,9 @@ import { ErrorComponent } from './layouts/error/error.component';
     // jhipster-needle-angular-add-module JHipster will add new module here
     JhpetclinicEntityModule,
     JhpetclinicAppRoutingModule,
+    LoadingBarModule,
+    LoadingBarHttpClientModule,
+    LoadingBarRouterModule,
   ],
   declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
   bootstrap: [MainComponent],
--- src/main/webapp/app/shared/constants/pagination.constants.ts
+++ src/main/webapp/app/shared/constants/pagination.constants.ts
@@ -1,2 +1,3 @@
 export const ITEMS_PER_PAGE = 20;
 export const MAX_PETS_PER_PAGE = 100;
+export const MAX_ITEMS_PER_PAGE = 100;
