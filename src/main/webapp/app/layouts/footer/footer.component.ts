2218936d2ff5af8d77b257d563e71a7ec647474e
Domagoj Madunić <domagoj.madunic@gmail.com>
2020-08-08 21:49:58 +0200

    Increased version to 0.4.0 and added build info to footer

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import { BUILD_TIMESTAMP } from 'app/app.constants';
+import * as moment from 'moment';
+import { Moment } from 'moment';
 
 @Component({
   selector: 'jhi-footer',
   templateUrl: './footer.component.html',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  buildTime: Moment;
+
+  constructor() {
+    this.buildTime = moment(+BUILD_TIMESTAMP!);
+  }
+}
